0cadce22e130bee42e0d39efc2f0683b641607a5
QuadnucYard <quadnucyard@gmail.com>
2025-01-16 21:58:48 +0800

    dev: test tasks
    fix

--- xmake.lua
+++ xmake.lua
@@ -1,34 +1,49 @@
+option("submit")
+option_end()
+
+local is_submit = has_config("submit")
+
 add_rules("mode.debug", "mode.release")
 
-set_plat("mingw")
-set_toolchains("llvm")
+if is_os("windows") then
+    set_plat("mingw")
+    set_toolchains("llvm")
+elseif is_os("linux") then
+    set_plat("linux")
+    set_toolchains("gcc")
+end
+
 set_languages("c++17")
-set_warnings("all")
+set_warnings("allextra")
 
-set_policy("build.sanitizer.address", true)
-set_policy("build.sanitizer.undefined", true)
+if not is_submit then
+    set_policy("build.sanitizer.address", true)
+    set_policy("build.sanitizer.undefined", true)
 
-add_requires("catch2")
+    add_requires("catch2")
+end
 
 target("fla-core")
     set_kind("static")
     add_includedirs("fla-project", { public = true })
-	add_files("fla-project/**/*.cpp")
+    add_files("fla-project/**/*.cpp")
 
 target("fla")
     set_kind("binary")
-	add_deps("fla-core")
-	add_files("fla-project/*.cpp")
-	set_rundir(os.projectdir())
+    add_deps("fla-core")
+    add_files("fla-project/*.cpp")
+    set_rundir(os.projectdir())
 
-for _, file in ipairs(os.files("tests/test_*.cpp")) do
-    local name = path.basename(file)
-    target(name)
-        set_kind("binary")
-        set_default(false)
-        add_deps("fla-core")
-		add_packages("catch2")
-        add_files("tests/" .. name .. ".cpp")
-        add_tests("default")
-        set_rundir("$(projectdir)")
+if not is_submit then
+    for _, file in ipairs(os.files("tests/test_*.cpp")) do
+        local name = path.basename(file)
+        target(name)
+            set_kind("binary")
+            set_default(false)
+            add_deps("fla-core")
+            add_packages("catch2")
+            add_files("tests/" .. name .. ".cpp")
+            add_tests("default")
+            set_rundir("$(projectdir)")
+    end
 end
